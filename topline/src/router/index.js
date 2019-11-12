25d69b35d40e4cc2c1553c21e2752eb52f1008ea
土豆想起了米饭 <5450488+top87@user.noreply.gitee.com>
2019-11-12 12:45:41 +0800

    登录页的绘制

--- topline/src/router/index.js
+++ topline/src/router/index.js
@@ -1,24 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 全局样式引入
+import '@/assets/css/global.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(ElementUI)
+
+const routes = [{
+  path: '/login',
+  name: 'login',
+  component: () =>
+    import('@/views/login')
+}]
 
 const router = new VueRouter({
   routes
---
+++ topline/src/views/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login-container">
+			<div class="login-box">
+        <el-form ref="loginFormRef" :model="loginForm">
+          <el-form-item>
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="loginForm.code"  placeholder="请输入校验码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="loginForm.xieyi"></el-checkbox>
+            <span>我已阅读并同意用户协议和隐私条款</span>
+          </el-form-item>
+          <el-form-item>
+            <el-button style="width:100%;" type="primary">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: blue;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .login-box {
+    width: 400px;
+    height: 320px;
+    background-color: pink;
+  }
+}
+</style>
